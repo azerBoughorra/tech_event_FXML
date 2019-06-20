6f98342928413bb3bd8cfb757e39a77c148d81a4
YoussefJaballi <youssef.jaballi@esprit.tn>
2019-06-20 15:10:41 +0200

    getlatest changes youssef

--- src/edu/esprit/GUI/HomeController.java
+++ src/edu/esprit/GUI/HomeController.java
@@ -6,10 +6,17 @@
 package edu.esprit.GUI;
 
 import edu.esprit.utils.UserManager;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
@@ -29,6 +36,17 @@ public class HomeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Button  b = new Button(UserManager.getUser().getRole().getDescription());
+        b.setOnAction(e->{
+        Parent root = null;
+            try {
+                root = FXMLLoader.load(getClass().getResource("form.fxml"));
+            } catch (IOException ex) {
+                Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                   homeContainer.getScene().setRoot(root);
+        });
+        
+        
         homeContainer.getChildren().add(b);
         // TODO
     }    
